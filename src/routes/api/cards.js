bc295e4fa4bb83607f79b92410c919752e363cef
Ken Jones <kenjwho@gmail.com>
2022-03-23 22:37:27 -0400

    Remove returnEtsyListingFromEtsyListingId

--- src/routes/api/cards.js
+++ src/routes/api/cards.js
@@ -2,27 +2,29 @@ import Airtable from 'airtable';
 
 import { variables } from '$lib/variables';
 
-import {returnFormattedBirds, returnEtsyListingFromEtsyListingId} from "$lib/utils"
+import { returnFormattedBirds } from '$lib/utils';
 
 export async function get() {
-  const base = new Airtable({apiKey: variables.AIRTABLE_API_KEY}).base(variables.AIRTABLE_BASE);
-  const table = base('Birds');
-  const records = await table.select({
-    view: "Grid view",
-    filterByFormula: "{Live On Site} = 1"
-  }).firstPage();
+	const base = new Airtable({ apiKey: variables.AIRTABLE_API_KEY }).base(variables.AIRTABLE_BASE);
+	const table = base('Birds');
+	const records = await table
+		.select({
+			view: 'Grid view',
+			filterByFormula: '{Live On Site} = 1'
+		})
+		.firstPage();
 
-  const data = returnFormattedBirds(records);
+	const data = returnFormattedBirds(records);
 
-  data.forEach(async (element, i)  => {
-    // if (element && element.etsyId) {
-    //   const listing = await returnEtsyListingFromEtsyListingId(element.etsyId);
-    //   data[i] = {listing};
-    // }
-  });
+	// data.forEach(async (element, i) => {
+	// 	if (element && element.etsyId) {
+	// 	  const listing = await returnEtsyListingFromEtsyListingId(element.etsyId);
+	// 	  data[i] = {listing};
+	// 	}
+	// });
 
-  return {
-    status: 200,
-    body: data
-  }
+	return {
+		status: 200,
+		body: data
+	};
 }
