c30ee053dd19c5eb0e5e0bf26af73e6b1a297569
KenJonesPizza <kenjwho@gmail.com>
2024-06-04 21:23:50 -0500

    Add PostHog Logic

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -4,8 +4,7 @@ import posthog from 'posthog-js';
 
 console.log('env:', env);
 export const load = async () => {
-	if (browser) {
-		console.log('env.PUBLIC_POSTHOG_KEY:', env.PUBLIC_POSTHOG_KEY);
+	if (browser && env.PUBLIC_POSTHOG_KEY) {
 		posthog.init(env.PUBLIC_POSTHOG_KEY, {
 			api_host: 'https://us.i.posthog.com',
 			person_profiles: 'always'
